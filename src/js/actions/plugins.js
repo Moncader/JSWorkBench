c5cbddd65e0326af49caf37cf0975c921a2eb8fa
Jason Parrott <jason.parrott@gree.co.jp>
2012-10-18 11:19:48 +0900

    Added plugins action with the update subaction to update plugins (and initialize them)

---
+++ src/js/actions/plugins.js
@@ -0,0 +1,65 @@
+(function(global) {
+
+  var initPlugin = global.plugins.init = function (pConfig, pName, pUpdate) {
+    var tPluginDir = pConfig.properties.pluginDir || 'jsworkbench-plugins';
+
+    if (!tPluginDir) {
+      return;
+    }
+
+    var tPlugin = pConfig.plugins[pName];
+
+    if (pUpdate || stat(tPluginDir + '/' + pName) === null) {
+      if (!pConfig.isQuiet) {
+        print('Initializing plugin ' + pName);
+      }
+
+      var tLocationHandlers = {};
+      global.fire('queryLocationHandlers', tLocationHandlers);
+
+      var tLocation = pConfig.expand(tPlugin.location);
+
+      if (!(tLocation in tLocationHandlers)) {
+        print('Invalid location type. The valid location types are:');
+        for (var k in tLocationHandlers) {
+          print('  ' + k);
+        }
+        return false;
+      }
+
+      var tHandler = new tLocationHandlers[tLocation](pConfig);
+      tHandler.setData(tPluginDir + '/' + pName, tPlugin);
+
+      if (tHandler.execute() === false) {
+        global.print('Failed to initialize plugin ' + pName);
+      }
+    }
+  }
+
+  var mActions = {
+    update: updatePlugins
+  };
+
+  function updatePlugins(pConfig) {
+    for (var tPluginName in pConfig.plugins) {
+      initPlugin(pConfig, tPluginName, true);
+    }
+  }
+
+  function pluginsAction(pConfig, pSubaction) {
+    if (!(pSubaction in mActions)) {
+      print('Subaction required.');
+      print('Subactions are:');
+      for (var k in mActions) {
+        print('  ' + k);
+      }
+    } else {
+      mActions[pSubaction](pConfig);
+    }
+  }
+
+  global.on('registerActions', function(pActions) {
+    pActions.plugins = pluginsAction;
+  });
+
+}(this));
--- src/js/plugins.js
+++ src/js/plugins.js
@@ -4,7 +4,6 @@
  * This code is licensed under the zlib license. See LICENSE for details.
  */
 
-
 (function(global) {
 
   var stat = global.stat;
@@ -19,35 +18,7 @@
     var tLocationHandlers = null;
 
     for (var tPluginName in pConfig.plugins) {
-      var tPlugin = pConfig.plugins[tPluginName];
-
-      if (stat(tPluginDir + '/' + tPluginName) === null) {
-        if (!pConfig.isQuiet) {
-          print('Initializing plugin ' + tPluginName);
-        }
-
-        if (tLocationHandlers === null) {
-          tLocationHandlers = {};
-          global.fire('queryLocationHandlers', tLocationHandlers);
-        }
-
-        var tLocation = pConfig.expand(tPlugin.location);
-
-        if (!(tLocation in tLocationHandlers)) {
-          print('Invalid location type. The valid location types are:');
-          for (var k in tLocationHandlers) {
-            print('  ' + k);
-          }
-          return false;
-        }
-
-        var tHandler = new tLocationHandlers[tLocation](pConfig);
-        tHandler.setData(tPluginDir + '/' + tPluginName, tPlugin);
-
-        if (tHandler.execute() === false) {
-          global.print('Failed to initialize plugin ' + tPluginName);
-        }
-      }
+      global.plugins.init(pConfig, tPluginName, false);
     }
 
     if (stat(tPluginDir) === null) {
